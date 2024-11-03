c3ba1418a9b52a81d3a4d4f71e4b205a56b93fba
Carlos-Ibaceta <carlos.ibaceta312@gmail.com>
2024-11-03 02:08:50 -0300

    ACC INVENTARIO <3

---
+++ Assets/Script/Inventario.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventario : MonoBehaviour
+{
+    public List<GameObject> Bag = new List<GameObject>();
+    public GameObject inv;
+    public bool Activar_inv;
+
+    public GameObject Selector;
+    public int ID;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Item"))
+        {
+            for (int i = 0; i < Bag.Count; i++)
+            {
+                Image bagImage = Bag[i].GetComponent<Image>();
+                if (bagImage != null && !bagImage.enabled)
+                {
+                    bagImage.enabled = true;
+                    bagImage.sprite = coll.GetComponent<SpriteRenderer>().sprite;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void Navegar()
+    {
+        if (Input.GetKeyDown(KeyCode.D) && ID < Bag.Count - 1)
+        {
+            ID++;
+        }
+        if (Input.GetKeyDown(KeyCode.A) && ID > 0)
+        {
+            ID--;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && ID > 3) // Cambié 'w' a 'W'
+        {
+            ID -= 4;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && ID < Bag.Count - 4) // Asegurarse de no salir del índice
+        {
+            ID += 4;
+        }
+
+        if (ID >= 0 && ID < Bag.Count)
+        {
+            Selector.transform.position = Bag[ID].transform.position;
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Navegar();
+
+        inv.SetActive(Activar_inv);
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Activar_inv = !Activar_inv;
+        }
+    }
+}
